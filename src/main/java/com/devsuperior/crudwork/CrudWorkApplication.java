05965eaaf7550e48d4e42dd5c753fafb375df5c6
Peres Julião <peresjuliao@gmail.com>
2020-10-15 17:44:39 -0300

    Project created

---
+++ src/main/java/com/devsuperior/crudwork/CrudWorkApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crudwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWorkApplication.class, args);
+	}
+
+}
